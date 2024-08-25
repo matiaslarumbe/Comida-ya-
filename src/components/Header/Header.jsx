d6e3fb6235f175f5f811d2741bbbf8dfd2128158
Matias Larumbe <matias_larumbe@hotmail.com>
2024-08-25 15:10:05 -0300

    Actualizacion y creacion de componentes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>pide tu comida favorita aquí</h2>
+            <p>Elija entre un menú diverso que ofrece una deliciosa variedad de platos elaborados con los mejores ingredientes y experiencia culinaria. Nuestra misión es satisfacer sus antojos y mejorar su experiencia gastronómica, una comida deliciosa a la vez.</p>
+            <button>Ver Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
